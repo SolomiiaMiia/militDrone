c8b41e900a746b8262208f3020d6bde3ada9f917
SolomiiaMiia <brigitska.solomiya@gmail.com>
2025-01-29 20:57:41 +0200

    upd

--- src/app/icons/icons.module.ts
+++ src/app/icons/icons.module.ts
@@ -1,10 +1,10 @@
 import { NgModule } from '@angular/core';
 
 import { FeatherModule } from 'angular-feather';
-import {TrendingUp , DollarSign, Compass, Command, Box, Camera, Bell, Monitor, Calendar, Clock,ShoppingCart, Dribbble,Linkedin, Facebook,Instagram, Twitter, Mail, Moon, Sun,} from 'angular-feather/icons';
+import {TrendingUp , DollarSign, Compass, Command, Box, Camera, Bell, Monitor, Calendar, Clock,ShoppingCart, Dribbble,Linkedin, Facebook,Instagram, Twitter, Mail, Moon, Sun, Youtube} from 'angular-feather/icons';
 
 const icons = {
-  TrendingUp , DollarSign, Compass, Command, Box, Camera, Bell, Monitor, Calendar, Clock,ShoppingCart, Dribbble,Linkedin, Facebook, Instagram, Twitter, Mail, Moon, Sun}
+  TrendingUp , DollarSign, Compass, Command, Box, Camera, Bell, Monitor, Calendar, Clock,ShoppingCart, Dribbble,Linkedin, Facebook, Instagram, Twitter, Mail, Moon, Sun, Youtube}
 
 
 @NgModule({
