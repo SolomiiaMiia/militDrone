c2eec6e7735fe0f2e8b29085b6e58d57c45b0ca3
SolomiiaMiia <brigitska.solomiya@gmail.com>
2025-02-16 22:14:30 +0200

    upd video

--- src/app/components/get-in-touch/get-in-touch.component.ts
+++ src/app/components/get-in-touch/get-in-touch.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import emailjs, { EmailJSResponseStatus } from '@emailjs/browser';
 
 @Component({
   selector: 'app-get-in-touch',
@@ -8,5 +9,25 @@ import { Component } from '@angular/core';
   styleUrl: './get-in-touch.component.scss'
 })
 export class GetInTouchComponent {
+
+  sendEmail(event: Event): void {
+    event.preventDefault(); // Запобігає перезавантаженню сторінки
+
+console.log( event.target as HTMLFormElement)
+    emailjs
+      .sendForm('service_h7w7lwg', 'template_osvtb1n', event.target as HTMLFormElement, {
+        publicKey: 'H8HfhrQ7I7g_hqm6q',
+      })
+      .then(
+        () => {
+          console.log('SUCCESS!');
+          alert('Ваше повідомлення було надіслано!');
+        },
+        (error) => {
+          console.log('FAILED...', (error as EmailJSResponseStatus).text);
+          alert('Помилка відправлення повідомлення!');
+        },
+      );
+  }
 
 }
